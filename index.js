c80f6938d2db6421726382ed56fc1fc28d998aa5
yuchin1189 <yuchin1189@proton.me>
2024-11-19 21:38:34 +0800

    initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+// 讀取 nev 檔內容
+import 'dotenv/config'
+// 引用 linebot
+import linebot from 'linebot'
+
+// 設定並建立機器人
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+bot.on('message', async (event) => {
+  // console.log(event)
+  if (event.message.type !== 'text') return
+
+  try {
+    const result = await event.reply(event.message.text)
+    console.log(result)
+  } catch (error) {
+    console.log(error)
+  }
+  // event.reply('hi').then(() => {
+  //   console.log(event.message.text)
+  // }).catch((error) => {
+  //   console.log(error)
+  // })
+})
+
+// 設定機器人監聽路徑和 port
+bot.listen('/', process.env.PORT || 3000, () => {
+  console.log('🤖✨')
+})
